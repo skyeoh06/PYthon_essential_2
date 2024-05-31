8e3d87dd3987a18fb0fc1356b7d9da88679376bc
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-05-31 13:57:15 +0800

    Create 2.1 LAB.py

---
+++ 2.1 LAB.py
@@ -0,0 +1,5 @@
+#What is BOM?
+>>BOM (Byte Order Mark) is a special combination of bits announcing the encoding used by a file's contents (e.g. UCS-4 or UTF-8).
+
+#Is Python 3 I18Ned?
+>>Yes, it's completely internationalized – we can use UNICODE characters inside our code, read them from the input and send them to the output.
