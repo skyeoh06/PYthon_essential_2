1786e35d7e785648d2bce1402c2157e5d245cd70
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-06-18 15:29:49 +0800

    Create 2.8.1.5 LAB.py

---
+++ 2.8.1.5 LAB.py
@@ -0,0 +1,12 @@
+# Which of the exceptions will you use to protect your code from being interrupted through the use of the keyboard?
+>> 
+KeyboardInterrupt
+
+# What is the name of the most general of all Python exceptions?
+>> 
+BaseException
+
+# Which of the exceptions will be raised through the following unsuccessful evaluation?
+huge_value = 1E250 ** 2
+>>
+OverflowError
