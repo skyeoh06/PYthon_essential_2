69ab745cd2d26fea1cb4059c390905618ab8a3f7
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-07-01 14:44:45 +0800

    Update 4.1 LAB.py

--- 4.1 LAB.py
+++ 4.1 LAB.py
@@ -316,7 +316,48 @@ f(1)=0
 f(2)=2
 
 
+#1.15
+#What is the expected output of the following code?
 
+class Vowels:
+    def __init__(self):
+        self.vow = "aeiouy "  # Yes, we know that y is not always considered a vowel.
+        self.pos = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.pos == len(self.vow):
+            raise StopIteration
+        self.pos += 1
+        return self.vow[self.pos - 1]
+
+
+vowels = Vowels()
+for v in vowels:
+    print(v, end=' ')
+>> a e i o u y
+
+#Write a lambda function, setting the least significant bit of its integer argument, and apply it to the map() function to produce the string 1 3 3 5 on the console.
+
+any_list = [1, 2, 3, 4]
+even_list = # Complete the line here.
+print(even_list)
+
+>> list(map(lambda n: n | 1, any_list))
+
+#What is the expected output of the following code?
+
+def replace_spaces(replacement='*'):
+    def new_replacement(text):
+        return text.replace(' ', replacement)
+    return new_replacement
+
+
+stars = replace_spaces()
+print(stars("And Now for Something Completely Different"))
+>> And*Now*for*Something*Completely*Different
 
 
 
